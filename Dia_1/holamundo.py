614e8e6987548443ed9d704c173dd244a320fef0
Adrian-YS <dnoxus2003@gmail.com>
2025-01-14 09:32:09 -0500

    dia_1

---
+++ Dia_1/holamundo.py
@@ -0,0 +1,33 @@
+"""print("1. Realice las siguientes operaciones. Los operandos son 3 y 4")
+#SUMA
+print(3 + 4)
+
+#RESTA
+print(4 - 3)
+
+#MULTIPLICACION
+print(3 * 4)
+
+#MODULO
+print(3 % 4)
+
+#DIVISION
+print(4 / 3)
+
+#EXPONENCIAL
+print(4 ** 3)
+
+print("2. Escribir cadenas. Las cadenas son las siguientes:")
+print('Adrian')
+print('Sanchez')
+print('Perú')
+
+print("3. Compruebe los tipos de datos de los siguientes datos:")
+print(type(10))
+print(type(9.8))
+print(type(4-4j))
+print(type(['Adrian','Python','Ecuador']))
+print(type('Adrian'))"""
+
+print(2 // 3)
+print(10 // 8)
